1fbabca3ece598c45dab9724ccaff0ca3de3dc96
Alexander Gock <alexander.gock@helmes.com>
2023-01-13 16:05:45 +0400

    Initial project structure setup

---
+++ city-list-back-end/src/main/java/com/alexandergock/citylist/CityListBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.alexandergock.citylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class CityListBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CityListBackEndApplication.class, args);
+  }
+
+}
---
+++ city-list-back-end/src/test/java/com/alexandergock/citylist/CityListBackEndApplicationTests.java
@@ -0,0 +1,11 @@
+package com.alexandergock.citylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class CityListBackEndApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
